540e33779aa46353708ac304b23cb634a726ebd8
Skilly <59287207+dhojka7@users.noreply.github.com>
2020-12-05 21:30:41 +0000

    Paralyze fix

--- Game/Entities/Player.Stats.cs
+++ Game/Entities/Player.Stats.cs
@@ -69,6 +69,9 @@ namespace RotMG.Game.Entities
 
         public float GetMovementSpeed()
         {
+            if (HasConditionEffect(ConditionEffectIndex.Paralyzed))
+                return 0;
+
             if (HasConditionEffect(ConditionEffectIndex.Slowed))
                 return MinMoveSpeed * MoveMultiplier;
 
